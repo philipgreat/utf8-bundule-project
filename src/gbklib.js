e8d59915a58b5f8e173acc7e64cedea22143d367
Philip Zhang <zhangxilai@doublechaintech.com>
2022-10-01 19:50:35 +0800

    🚀remove unused code

--- src/gbklib.js
+++ src/gbklib.js
@@ -1,14 +1,4 @@
 const { encode, decode } = require('fast-gbk')();
 
-const str = '你好，世界！';
- 
-const arr = encode(str);
- 
-arr.forEach(v=>{
-    //console.log(v);
-})
-
-//console.log(decode(arr));
-
 window.encodeToGBKArray = encode;
 window.decodefromGBKArray = decode;
